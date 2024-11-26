e9c20ef4fbdb0c6f471f77199ff350493801dde3
jaonorton <jaonorton@gmail.com>
2024-11-26 16:17:09 -0300

    Inicio js

---
+++ script.js
@@ -0,0 +1,46 @@
+const argentina = [
+    { nome: "Plano Familiar p/ 4 pessoas", preco: "R$ 2000,00", parcelas: "12x de R$200,00" },
+    { nome: "Plano casal", preco: "R$ 1200,00", parcelas: "12x de R$120,00" },
+    { nome: "Plano Individual", preco: "R$ 699,90", parcelas: "12x de R$69,90" },
+    { nome: "Plano customizado", preco: "A combinar", parcelas: "" }
+ ];
+ 
+ 
+ const cuba = [
+    { nome: "Plano Familiar p/ 4 pessoas", preco: "R$ 2500,00", parcelas: "12x de R$250,00" },
+    { nome: "Plano casal", preco: "R$ 1400,00", parcelas: "12x de R$140,00" },
+    { nome: "Plano Individual", preco: "R$ 899,90", parcelas: "12x de R$89,90" },
+    { nome: "Plano customizado", preco: "A combinar", parcelas: "" }
+ ];
+ 
+ 
+ const indonesia = [
+    { nome: "Plano Familiar p/ 4 pessoas", preco: "R$ 3000,00", parcelas: "12x de R$300,00" },
+    { nome: "Plano casal", preco: "R$ 1800,00", parcelas: "12x de R$180,00" },
+    { nome: "Plano Individual", preco: "R$ 1199,00", parcelas: "12x de R$119,90" },
+    { nome: "Plano customizado", preco: "A combinar", parcelas: "" }
+ ];
+ 
+ 
+ const italia = [
+    { nome: "Plano Familiar p/ 4 pessoas", preco: "R$ 3500,00", parcelas: "12x de R$350,00" },
+    { nome: "Plano casal", preco: "R$ 2300,00", parcelas: "12x de R$230,00" },
+    { nome: "Plano Individual", preco: "R$ 1799,00", parcelas: "12x de R$179,90" },
+    { nome: "Plano customizado", preco: "A combinar", parcelas: "" }
+ ];
+ 
+ 
+ const franca = [
+    { nome: "Plano Familiar p/ 4 pessoas", preco: "R$ 4000,00", parcelas: "12x de R$400,00" },
+    { nome: "Plano casal", preco: "R$ 3200,00", parcelas: "12x de R$320,00" },
+    { nome: "Plano Individual", preco: "R$ 2699,90", parcelas: "12x de R$269,90" },
+    { nome: "Plano customizado", preco: "A combinar", parcelas: "" }
+ ];
+
+ let select = 0
+ const nav2 = document.getElementById("sidebar2");
+ const nav2_conteudo = document.getElementById("lista-pacotes");
+ const nav3 = document.getElementById("sidebar3");
+ const nav3_conteudo = document.getElementById("pacote-conteudo");
+
+ //https://github.com/C9BrenoFR/AulaDevWebSemanaDaComputacao/tree/main
